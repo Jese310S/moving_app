3a4a1321bd91512d2c74cff9effb7f2900a7fe9b
Jese310S <jess.sole710@gmail.com>
2020-08-06 14:59:06 +1000

    fix syntax bug in create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session #reset user id for security
-      log_in users
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
