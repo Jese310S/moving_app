be5a135e85db9bce2a9cf03104fd00495c5b4e14
Jese310S <jess.sole710@gmail.com>
2020-08-06 11:27:58 +1000

    create login form and session for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log in user and redirect to user show page
+    else
+      flash[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
