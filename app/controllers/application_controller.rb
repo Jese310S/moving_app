6d070d302904e9e5deb151d37e1ed9ed8a0fea0b
Jese310S <jess.sole710@gmail.com>
2020-08-06 12:36:48 +1000

    change redirect in login session for security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
+    include SessionsHelper
 
-    def hello
-        render html: 'hello, world'
-    end
+    # def hello
+    # end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #log in user and redirect to user show page
+      reset_session #reset user id for security
+      log_in users
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
