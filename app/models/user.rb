ebfb7d923dfdf9dcd47b6a5aaf8d28b776d48277
Jese310S <jess.sole710@gmail.com>
2020-08-06 15:11:29 +1000

    add more validation for user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ApplicationRecord
     validates(:email, presence: true, length: { maximum: 255},
                         format: { with: VALID_EMAIL_REGEX })
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 
+
+    #returns the hash digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST :
+                                                      Bcrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+    end
 end
