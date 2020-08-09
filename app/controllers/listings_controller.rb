e66af4177c52944faaca3009f14dee91231dfe34
Jese310S <jess.sole710@gmail.com>
2020-08-09 19:22:24 +1000

    add destroy for listings and a listings feed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,6 @@
 class ListingsController < ApplicationController
+    # before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user, only: :destroy
    
     def create
         @listing = current_user.listings.build(listing_params)
@@ -11,13 +13,24 @@ class ListingsController < ApplicationController
     end
 
     def destroy
-   
+        @listing.destroy
+        flash[:success] = "Listing deleted"
+        if request.referrer.nil? || request.referrer == listings_url
+            redirect_to root_url
+            else
+            redirect_to request.referrer
+            end
     end
 
     private
 
 #strong params that permits only listing's content
     def listing_params
-        params.require(:listing).permit(:content)
+        params.require(:listing).permit(:title)
     end
+
+    def correct_user
+        @listing = current_user.listings.find_by(id: params[:id])
+        redirect_to root_url if @listing.nil?
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
 
+    def feed
+        listings
+    end
 end
